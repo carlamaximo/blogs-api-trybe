bce574cc52381d5845b3f5c4670691fbe29563a0
Carla <carladmaximo@gmail.com>
2022-03-26 17:13:44 -0300

    tentativa passar avaliadoro remoto req 01

--- migrations/20220326200122-create-blog-post.js
+++ migrations/20220326200122-create-blog-post.js
@@ -8,17 +8,14 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-
       title: {
         allowNull: false,
         type: Sequelize.STRING,
       },
-
       content: {
         allowNull: false,
         type: Sequelize.STRING,
       },
-
       userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -29,12 +26,10 @@ module.exports = {
           key: 'id',
         },
       },
-
       published: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      
       updated: {
         allowNull: false,
         type: Sequelize.DATE
--- models/User.js
+++ models/User.js
@@ -1,20 +1,28 @@
-const associate = (models) => {
-  models.User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'posts' });
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  displayName: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+  image: DataTypes.STRING,
 };
 
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', 
-  {
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING,
-  },
-  {
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', Attributes, {
+    tableName: 'Users',
     timestamps: false,
   });
 
-  User.associate = associate;
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreingKey: 'userId', as: 'posts' });
+  };
 
   return User;
-}; 
+};
